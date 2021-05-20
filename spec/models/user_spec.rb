3cde284a5cc7def00ce04c55377960ac955968da
nobonobonobo <82573159+nobonobonobo@users.noreply.github.com>
2021-05-20 21:29:50 +0900

    モデルの単体テストコードを記述

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+
+  describe 'ユーザー新規登録' do
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
+    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+      @user.password = '000000'
+      @user.password_confirmation = '000000'
+      expect(@user).to be_valid
+    end
+    it 'nicknameが空では登録できない' do
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.error.full_messages).to include "Nickname can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ""
+      @user.valid?
+      expect(@user.error.full_messages).to include "Email can't be blank"
+    end
+    it 'passwordが空では登録できない' do
+      @user.password = ""
+      @user.valid?
+      expect(@user.error.full_messages).to include "Password can't be blank"
+    end
+    it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.error.full_messages).to include "Password_confirmation can't be blank"
+    end
+    it '重複したemailが存在する場合登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+    it 'passwordは、半角英数字が混合されていれば登録できる' do
+      @user.password = "000000", "aaaaaa", "ａａａａａａ"
+      @user.valid?
+      expect(@user.error.full_messages).to include ""
+    end
+  end
+end
