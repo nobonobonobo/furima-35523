67e4d908b3d80a8a1e12d658a34f75269cb01195
nobonobonobo <82573159+nobonobonobo@users.noreply.github.com>
2021-05-27 12:04:31 +0900

    Active Storageの導入

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -2,4 +2,10 @@ class FurimasController < ApplicationController
   def index
     #@furimas = Furima.all
   end
+
+  private
+
+  def furima_params
+    params.require(:furima).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
